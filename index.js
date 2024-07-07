33e4943902272cf1b3fc7f23b590bd45d9846d96
peter <peter.rono@student.moringaschool.com>
2024-07-07 12:22:04 +0300

    Used a for loop to iterate over the names array

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+function countDown(number){
+    while (number >= 0){
+        console.log(number);
+        number --;
+    }
+}
+//Call the funtion 
+countDown(10);
+
+
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+      // The 'thank you' message for each name
+      const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+      // Now add the message
+      messages.push(message);
+    }
+    return messages;
+  }
+  
+  //call the funtion;
+  const result = writeCards(["Rono", "Charlie", "Kendrick"], "Prom");
+
+  console.log(result);
+  
